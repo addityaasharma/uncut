0e321c3b77199d3e3e6b4a64d78e0dd75fe90eb3
addityaasharma <adityasharma96655@gmail.com>
2025-10-17 11:21:30 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import SignupPage from './pages/SignupPage'
 import ForgotPassword from './pages/ForgotPassword'
+import SocialFeed from './pages/SocialFeed'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<SignupPage />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/dashboard" element={<SocialFeed />} />
     </Routes>
     </>
   )
---
+++ src/pages/SocialFeed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialFeed = () => {
+  return (
+    <div>SocialFeed</div>
+  )
+}
+
+export default SocialFeed
